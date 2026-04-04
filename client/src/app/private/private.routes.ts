8b856b3337f3f6e25e51da98e02ad9f7e7665efe
enrique <enriqueonaga@gmail.com>
2026-04-04 20:42:17 -0300

    chore: rotas da aplicação

---
+++ client/src/app/private/private.routes.ts
@@ -0,0 +1,18 @@
+import { Routes } from '@angular/router';
+
+export const PRIVATE_ROUTES: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./main/main')
+        .then(m => m.Main),
+    children: [
+      {
+        path: 'dashboard',
+        loadComponent: () =>
+          import('./pages/dashboard/dashboard')
+            .then(m => m.Dashboard)
+      },
+    ]
+  }
+];
